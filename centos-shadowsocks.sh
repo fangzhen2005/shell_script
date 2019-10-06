cebc70bc93dccfc390a287824cd5ecc13e874348
fangzhen2005 <fangzhen_1986@163.com>
2019-10-06 19:43:18 +0800

    Create centos-shadowsocks.sh

---
+++ centos-shadowsocks.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+####################################
+install_home="/etc/shadowsocks/" # 安装目录
+port=$1;                         # 端口
+password=$2                      # 密码
+encrypt=$3                       # 加密方式
+####################################
+
+if [ -n "$1" ]     # -n 参数测试 $1(第一个参数)是否为null 
+   then
+       port="2019"
+   else
+       echo "port is $1"
+fi
+
+if [ -n "$2" ]     # -n 参数测试 $2(第一个参数)是否为null 
+   then
+       password="333444"
+   else
+       echo "password is $2"
+fi
+
+if [ -n "$3" ]     # -n 参数测试 $3(第一个参数)是否为null 
+   then
+       encrypt="chacha20-ietf-poly1305"
+   else
+       echo "encrypt is $3"
+fi
+
+systemctl stop firewalld
+systemctl disable firewalld
+
+rm -rf $install_home
+mkdir -p $install_home
+
+yum update -y
+
+yum -y install epel-release
+yum install -y python-pip git gcc gcc-c++ make
+pip install --upgrade pip
+pip install setuptools
+# pip install shadowsocks
+pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+
+cd $install_home
+
+cat > $install_home/config.json <<EOF
+{
+    "server":"0.0.0.0",
+    "server_port":$port,
+    "password":"$password",
+    "method":"$encrypt"
+}
+EOF
+
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.15/libsodium-1.0.15.tar.gz
+tar zxvf libsodium-1.0.15.tar.gz
+cd libsodium-1.0.15
+./configure
+make && make install 
+
+echo "/usr/local/lib" > /etc/ld.so.conf.d/usr_local_lib.conf
+ldconfig
+
+start_cmd="ssserver -c "$install_home"config.json -d start"
+$start_cmd
+echo 'shadowsocks 服务已启动'
+sleep 3
+
+echo $start_cmd >> /etc/rc.local
+chmod +x /etc/rc.d/rc.local
+
+echo "安装成功,已加入开机自启,端口:$port 密码:$password 加密方式:$encrypt"
